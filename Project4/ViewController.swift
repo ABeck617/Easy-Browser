4416b8eae2c01f3f6ecf51986e697b0a471ea854
Anthony Beckford <abeckford532@gmail.com>
2022-10-29 07:44:56 -0400

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Anthony Beckford on 10/29/22.
+//
+
+import UIKit
+import WebKit
+
+
+
+class ViewController: UIViewController {
+    // creating a property to reference it later on
+    var webView: WKWebView!
+    
+    
+    // adding a method
+    override func loadView() {
+        <#code#>
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
